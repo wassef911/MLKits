df43bbd6e6a9fcc04299380647a9cfc0dd4feccf
wassef911 <1.wassef911@gmail.com>
2021-02-17 22:45:32 +0100

    moved knn to separate fn

--- plinko/score.js
+++ plinko/score.js
@@ -1,5 +1,4 @@
 const outputs = [];
-const predictionPoint = 300;
 const k = 3;
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
@@ -7,10 +6,23 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-  const bucket = _.chain(outputs).map(row => [distance(row[0], row[3])]).sortBy(row => row[0]).slice(0, k).countBy(row => row[1]).toPairs().sortBy(row => row[1]).last().first().parseInt().value();
-  console.log("Your point will probably fall into", bucket);
+  const bucket =
+    console.log("Your point will probably fall into", bucket);
 }
-function distance(point) {
-  return Math.abs(point - predictionPoint);
+function knn(data, point) {
+  return _.chain(data).map(row => [distance(row[0], point)]).sortBy(row => row[0]).slice(0, k).countBy(row => row[1]).toPairs().sortBy(row => row[1]).last().first().parseInt().value();
+}
+
+function distance(pointA, pointB) {
+  return Math.abs(pointA - pointB);
+}
+
+function splitDataset(data, testCount) {
+  const shuffled = _.shuffle(data);
+
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+
+  return [testSet, trainingSet];
 }
 
